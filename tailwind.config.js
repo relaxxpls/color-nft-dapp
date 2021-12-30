acaaaafc922b1e0425ebcccad032d8b5577b0259
Laxman Desai <lolopaw24@gmail.com>
2021-12-30 15:40:48 +0530

    👌 IMPROVE: Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      mono: ['"Fira Code"', 'SFMono-Regular'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
